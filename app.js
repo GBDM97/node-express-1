0ebbbfaa58f4670c3e820a80eed517f3088ed3f7
GBDM97 <ghian.m@gmail.com>
2022-11-10 13:59:04 -0300

    modules test

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const m1 = require('./modules/m1')
 
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.send("Hello World!" + m1.sum1(1 , 2));
 });
 
 app.listen(port, function () {
---
+++ modules/m1.js
@@ -0,0 +1,9 @@
+module.exports = {
+    sum1(n , n2) {
+      return n + n2;
+    },
+  
+    rem1(n , n2) {
+      return n % n2;
+    },
+  };
